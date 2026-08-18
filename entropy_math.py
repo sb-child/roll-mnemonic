8c1ed334af12aabd8166534ebb62a6a79bf703b4
sbchild <sbchild0@gmail.com>
2026-08-19 00:07:28 +0800

    fix math issue

--- entropy_math.py
+++ entropy_math.py
@@ -29,8 +29,8 @@ def shannon(data: bytes | str) -> float:
 
 
 def curve_1(y: float) -> float:
-    if y >= 8:
-        y = 8
+    if y >= 7.9:
+        y = 7.9
     elif y < 0.5:
         y = 0.5
     return 100 - math.log((8 - y) / y) / math.log(1.02)
